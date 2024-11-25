Corrected a bug where C_XY did not have the correct factor of 1/2 for ST interference contributions

physics/fccee_scattering/eeee.py
@@ -83,7 +83,7 @@ def sigma_eeee_tot(wc_obj, par, E):
         geX = geX_dict[X]
         geX2 = geX**2 # Define powers of couplings to speed up evaluation
         geX4 = geX**4
-        CXY = wc_4e[f'{X}{X}'] 
+        CXY = wc_4e[f'{X}{X}'] / 2
         contributions[f'ST_{X}{X}'] = 2*np.real((((27*s*(3*e2*s + geX2*(2*mZ*((-1j)*GammaZ + mZ) + 3*s)))/10. - (6j)*geX2*((-1j)*GammaZ*mZ + mZ2 + s)**2*np.arctan((360*GammaZ*mZ*s)/(400*mZ**4 + 19*s**2 + 400*mZ2*(GammaZ2 + s))) + (3429*s**3*np.conjugate(CXY))/1000. - 6*e2*s**2*np.log(19) + 3*geX2*((-1j)*GammaZ*mZ + mZ2 + s)**2*np.log((GammaZ2*mZ2 + (mZ2 + s/20.)**2)/(GammaZ2*mZ2 + (mZ2 + (19*s)/20.)**2)))*F_eeee_s_channel(wc_4e, X, X, s))/6.)
 
     sigma = 0
@@ -166,7 +166,7 @@ def AFB_eeee(wc_obj, par, E):
         geX = geX_dict[X]
         geX2 = geX**2 # Define powers of couplings to speed up evaluation
         geX4 = geX**4
-        CXY = wc_4e[f'{X}{X}'] 
+        CXY = wc_4e[f'{X}{X}'] / 2
         contributions[f'ST_{X}{X}'] = 2*np.real(((162*(e2 + geX2)*s**2 - (800j)*geX2*((-1j)*GammaZ*mZ + mZ2 + s)**2*np.arctan((18*GammaZ*mZ*s)/(40*mZ**4 + s**2 + mZ2*(40*GammaZ2 + 22*s))) + 324*s**3*np.conjugate(CXY) + 400*((2j)*geX2*((-1j)*GammaZ*mZ + mZ2 + s)**2*np.arctan((18*GammaZ*mZ*s)/(40*mZ**4 + 19*s**2 + mZ2*(40*GammaZ2 + 58*s))) - 2*e2*s**2*np.log(5.2631578947368425) + geX2*((-1j)*GammaZ*mZ + mZ2 + s)**2*(np.log(GammaZ2*mZ2 + (mZ2 + s/20.)**2) - 2*np.log(GammaZ2*mZ2 + (mZ2 + s/2.)**2) + np.log(GammaZ2*mZ2 + (mZ2 + (19*s)/20.)**2))))*F_eeee_s_channel(wc_4e,X,X,s))/800.)
 
     sigma_FB = 0 #Forward - backward cross-section

physics/fccee_scattering/eell.py
@@ -52,6 +52,8 @@ def F_eell_SM(l1, Xl1, l2, Xl2, s, wc_obj, par):
     elif Xl2 == 'R':
         gZl2 = gVl2 - gAl2
     # SM contribution
+    print("SM photon contribution: ", g2*s2w * Ql * Ql / s)
+    print("SM Z contribution: ", gZl1 * gZl2 / (s - mZ**2 + 1j * mZ * GammaZ))
     return g2*s2w * Ql * Ql / s + gZl1 * gZl2 / (s - mZ**2 + 1j * mZ * GammaZ)
 
 # This is sm-like in the sense that it only uses SM-like vertices; but the Zff couplings are modified by SMEFT corrections
@@ -76,10 +78,14 @@ def wceff_eell_np(wc_obj, par, scale):
     # match to notation used in the observable
     # Note that the approach here is slightly different from flavio.physics.dileptons.ppll.py in that we add hermitian conjugates explicitly into the wc dictionary
     # CVXX_l (8 entries, 4 for each lepton):
-    wc['CVLL_mu'] = wcxf_dict.get('ll_1122',0) +  wcxf_dict.get('ll_1221',0)
-    wc['CVRR_mu'] = wcxf_dict.get('ee_1122',0)
+    wc['CVLL_mu'] =  wcxf_dict.get('ll_1122',0) +  wcxf_dict.get('ll_1221',0)
+    print('CVLL_mu =', wc['CVLL_mu'])
+    wc['CVRR_mu'] =  wcxf_dict.get('ee_1122',0)
+    print('CVRR_mu =', wc['CVRR_mu'])
     wc['CVLR_mu'] = wcxf_dict.get('le_1122',0)
+    print('CVLR_mu =', wc['CVLR_mu'])
     wc['CVRL_mu'] = wcxf_dict.get('le_2211',0)
+    print('CVRL_mu =', wc['CVRL_mu'])
 
     wc['CVLL_tau'] = wcxf_dict.get('ll_1133',0) +  wcxf_dict.get('ll_1331',0)
     wc['CVRR_tau'] = wcxf_dict.get('ee_1133',0)
@@ -146,8 +152,8 @@ def sigma_eell_tot(wc_dict, s, l):
             key = 'VV{}{}_{}'.format(X, Y, l)
             if key in f_integrated:
                 sigma += 1 / (16 * pi * s**2) * f_integrated['VV{}{}_{}'.format(X, Y, l)](s) * np.abs(wc_dict['CV{}{}_{}'.format(X,Y,l)])**2 
-                #print('Added', key, 'to total cross-section')
-                #print('sigma =', sigma, 'at this point')
+                print('Added', key, 'to total cross-section')
+                print('sigma =', sigma, 'at this point')
     # Return total cross-section
     sigma += 1 / (16 * pi * s**2) * f_integrated['non_interfering_{}'.format(l)](s) * np.abs(wc_dict['C_non_interfering_{}'.format(l)])**2
     return sigma